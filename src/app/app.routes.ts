d823e428a73daf7272bf1443906cd45e3e7d06c4
cinnnk@gmail.com <qin@pollex.com.tw>
2024-09-19 00:04:58 +0800

    day4 toggleDirective  補

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { AppComponent } from './app.component';
 import { ButtonThemeComponent } from '../day-1/button-theme/button-theme.component';
 import { CatalogComponent } from '../catalog/catalog.component';
 import { Day2PageComponent } from '../day-2/day2-page/day2-page.component';
-import { DialogComponent } from './day-3/dialog/dialog.component';
 import { Day3PageComponent } from './day-3/day-3-page/day-3-page.component';
+import { Day4PageComponent } from './day-4/day-4-page/day-4-page.component';
 
 export const routes: Routes = [
   {
@@ -28,7 +28,11 @@ export const routes: Routes = [
           {
             path: 'day-3/dialog',
             component: Day3PageComponent,
-          }
+          },
+          {
+            path: 'day-4/toggle',
+            component: Day4PageComponent,
+          },
         ]
       },
 
