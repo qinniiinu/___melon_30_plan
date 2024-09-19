67a9288c8671f4834bf13b9e4bed0a596ca74c90
cinnnk@gmail.com <qin@pollex.com.tw>
2024-09-20 04:21:47 +0800

    fix: route亂寫

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,9 @@ import { Day2PageComponent } from '../day-2/day2-page/day2-page.component';
 import { Day3PageComponent } from './day-3/day-3-page/day-3-page.component';
 import { Day4PageComponent } from './day-4/day-4-page/day-4-page.component';
 import { Day5PageComponent } from './day-5/day-5-page/day-5-page.component';
+import { Day6PageComponent } from './day-6-page/day-6-page.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: AppComponent,
-  },
 
   {
     path: 'day-1/button-color',
@@ -33,6 +30,10 @@ export const routes: Routes = [
   {
     path: 'day-5/screen-width',
     component: Day5PageComponent,
+  },
+  {
+    path: 'day-6/linePay',
+    component: Day6PageComponent,
   },
 
 
