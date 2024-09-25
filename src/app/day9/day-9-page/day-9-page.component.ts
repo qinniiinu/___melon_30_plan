57c534f689fbeb2d7688cd8f42f58510e12e3a59
cinnnk@gmail.com <qin@pollex.com.tw>
2024-09-26 04:19:32 +0800

    計時器 增加重新開始、暫停、設定秒數

--- src/app/day9/day-9-page/day-9-page.component.ts
+++ src/app/day9/day-9-page/day-9-page.component.ts
@@ -1,5 +1,14 @@
 import { DatePipe } from '@angular/common';
-import { Component, computed, effect, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  effect,
+  inject,
+  model,
+  NgZone,
+  signal,
+} from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-day-9-page',
@@ -11,29 +20,92 @@ import { Component, computed, effect, signal } from '@angular/core';
 export class Day9PageComponent {
   $timer = signal<Date>(new Date());
   $finalTime = signal<Date>(new Date());
-  remain = computed(
-    () => {
-      const thousandSeconds = this.$finalTime().getTime() - this.$timer().getTime()
-      return thousandSeconds
-    }
-  );
-  interval: NodeJS.Timeout;
+  remain = computed(() => {
+    const thousandSeconds =
+      this.$finalTime().getTime() - this.$timer().getTime();
+
+    return thousandSeconds;
+  });
+
+  $isPause = signal<boolean>(true);
+  interval!: NodeJS.Timeout;
+  modelInput = model<number | string>(100);
+  private _snackBar = inject(MatSnackBar);
+
   constructor() {
-    this.initFinalTime()
-      this.interval = setInterval(() => {
-        this.$timer.update(()=>new Date());
-      }, 1000);
+    this.initFinalTime(this.modelInput() as number);
+    const zone = inject(NgZone);
+    zone.runOutsideAngular(() => {
+      this.timerStart();
+    });
+
+    effect(() => {
+      if (this.remain() <= 0) {
+        this.openSnackBar('時間到', 'Qaq');
+        this.timerPause();
+        this.timerReStart();
+      }
+    });
   }
 
-  initFinalTime() {
+  timerReStart() {
+    this.initFinalTime(this.modelInput() as number);
+    this.timerStart()
+  }
+
+  private timerStart() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+    //一秒一次刷新現在時間
+    this.interval = setInterval(() => {
+      this.$timer.update(() => new Date());
+    }, 1000);
+    this.$isPause.set(false);
+  }
+
+  private timerPause() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+    this.$isPause.set(true);
+  }
+
+  initFinalTime(val: number) {
     const now = new Date(); // 獲取當前時間
-    const finalTime = new Date(now.getTime() + 30 * 60000); //30 分鐘後時間
+    const finalTime = new Date(now.getTime() + val * 1000); //秒數
     this.$finalTime.set(finalTime);
   }
 
   ngOnDestroy(): void {
-    if(this.interval ){
-      clearInterval(this.interval)
+    if (this.interval) {
+      clearInterval(this.interval);
     }
+  }
+
+  // 暫停中按下
+  onPause() {
+    if (this.$isPause()) {
+      //重新計算結束時間
+      this.$finalTime.update((curr) => {
+        const newFinalTime = new Date().getTime() + this.remain();
+        return new Date(newFinalTime);
+      });
+      // 重啟計時
+      this.$timer.update(() => new Date());
+      this.interval = setInterval(() => {
+        this.$timer.update(() => new Date());
+      }, 1000);
+    } else {
+      if (this.interval) {
+        //清除一秒一次刷新
+        clearInterval(this.interval);
+      }
+    }
+    this.$isPause.update((curr) => !curr);
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action);
   }
 }
